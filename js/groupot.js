dc44006d3cf7aa5c092c826ef7d12a0c363eb3ac
Jorge Alvaro <jorge.alvaro@buongiorno.com>
2013-05-08 21:03:26 +0200

    sendByEmail function created. can be tested from create event form

--- js/groupot.js
+++ js/groupot.js
@@ -4,13 +4,13 @@ function loadContacts() {
 	var activity = new MozActivity({
 		// Ask for the "pick" activity
 		name: "pick",
- 
+
 		// Provide de data required by the filters of the activity
 		data: {
 			type: "webcontacts/email"
 		}
 	});
- 
+
 	activity.onsuccess = function() {
 		var contact = this.result;
 		console.log("A contact has been retrieved");
@@ -20,11 +20,11 @@ function loadContacts() {
 			console.log(contact[i]);
 		}
 		li = document.createElement('li');
-		
+
 		spanText = document.createElement('span');
 		spanText.appendChild (document.createTextNode(contact["name"]));
 		spanText.className = "left";
-		
+
 		li.appendChild(spanText);
 
 		ul = document.getElementById('contacts');
@@ -32,17 +32,16 @@ function loadContacts() {
 
 		contacts.push(contact);
 	};
- 
+
 	activity.onerror = function() {
 		console.log(JSON.stringify(this));
 	};
 }
 
-function getSettlement(currentEvent) {
-	console.log(JSON.stringify(currentEvent));
+function getUserBalance(currentEvent) {
 	var userBalance = {};
-	for(userId in currentEvent.users) {
-		var user = currentEvent.users[userId];
+	for(userId in currentEvent.contacts) {
+		var user = currentEvent.contacts[userId];
 		userBalance[user] = 0;
 	}
 	var payments = currentEvent.payments;
@@ -59,9 +58,15 @@ function getSettlement(currentEvent) {
 	for(i in userBalance) {
 		userBalance[i] = roundCents(userBalance[i]);
 	}
+	return userBalance;
+}
 
-	console.log(JSON.stringify(userBalance));
+function getSettlement(currentEvent) {
+	var userBalance = getUserBalance(currentEvent);
+	return getSettlementsFromBalance(userBalance);
+}
 
+function getSettlementsFromBalance(userBalance) {
 	var settlement = [];
 	if(!isSettled(userBalance)) {
 		for(i in userBalance) {
@@ -91,6 +96,7 @@ function getSettlement(currentEvent) {
 		userBalance[max] = roundCents(userBalance[max]-amount);
 	}
 	console.log(JSON.stringify(settlement));
+	return settlement;
 }
 
 function isSettled(userBalance) {
@@ -125,6 +131,77 @@ function createEvent() {
 	var name = document.getElementById("eventname").value;
 	console.log(name);
 	console.log(JSON.stringify(contacts));
+
+	var event = {
+		"name": name,
+		//"contacts": contacts,
+		"contacts": ["me", "a", "b", "c"],
+		"payments": [
+			{ "from": "me", "to": ["a", "b"], "amount": 10, "name": "gas" },
+			{ "from": "a", "to": ["a", "b"], "amount": 10, "name": "food" }
+		]
+	};
+	// ToDo
+	// save event
+	// go to event page
+	sendEventByEmail(event);
+}
+
+function sendEventByEmail(event) {
+	var to = "";
+	for(contactId in event.contacts) {
+		to += event.contacts[contactId].email;
+	}
+
+	var subject = "[groupot] Summary of the event " + event.name;
+
+	var text = "This is the summary of the event named " + event.name + "\n";
+
+	text += "\nExpense details:\n";
+	for(paymentId in event.payments) {
+		text += event.payments[paymentId].name + " (" + event.payments[paymentId].amount + " from " + event.payments[paymentId].from + " to " + event.payments[paymentId].to + ")" + "\n";
+	}
+
+	text += "\nBalance by user:\n";
+
+	var userBalance = getUserBalance(event);
+	for(userBalanceId in userBalance) {
+		if(userBalance[userBalanceId] > 0) {
+			text += userBalanceId + " paid " + userBalance[userBalanceId] + " more than needed\n";
+		} else if(userBalance[userBalanceId] < 0) {
+			text += userBalanceId + " paid " + userBalance[userBalanceId] + " less than needed\n";
+		} else {
+			text += userBalanceId + " paid just what was needed\n";
+		}
+	}
+
+	text += "\nRecommended payments to settle the event:\n";
+
+	var settlement = getSettlementsFromBalance(userBalance);
+	console.log(JSON.stringify(settlement));
+	for(settlementId in settlement) {
+		text += settlement[settlementId].from + " should give " + settlement[settlementId].amount + " to " + settlement[settlementId].to + "\n";
+	}
+
+	text += "\ngroupot: Accounting for groups made easy";
+
+	var activity = new MozActivity({
+		name: "new",
+		data: {
+			type: "mail",
+			URI: "mailto:"+to+"?subject="+encodeURIComponent(subject)+"&body="+encodeURIComponent(text)
+		}
+	});
+
+	activity.onsuccess = function() {
+		console.log("onsuccess");
+		console.log(JSON.stringify(this));
+	}
+
+	activity.onerror = function() {
+		console.log("onerror");
+		console.log(JSON.stringify(this));
+	}
 }
 
 function addEventListeners() {
